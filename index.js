02446eb223a298447114e6bd6f56b378c886e3ae
Neil <neillobo@outlook.com>
2014-08-27 20:47:32 -0400

    removed node_modules, added mongoose

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://readitlive:rilonlymenotme2398@capital.0.mongolayer.com:10030,capital.1.mongolayer.com:10030/readitlive2');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback () {
+  // yay!
+  console.log("YAAAY");
+});
 
 app.get('/', function(req, res){
   res.sendFile('index.html',{root: '/root/liveblog/'});
