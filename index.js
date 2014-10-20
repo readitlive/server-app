c3974a58950e8803ea6122f524fcbd733e936a29
neillobo <neillobo@outlook.com>
2014-10-20 12:40:29 -0700

    bidirectional commenting and retrieving events

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ io.adapter(redis({ host: 'localhost', port: 6379 }));
 //connecting to remote MongoDB database using Mongoose ORM
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://readitlive:rilonlymenotme2398@capital.0.mongolayer.com:10030,capital.1.mongolayer.com:10030/readitlive2');
+mongoose.connect('mongodb://readitlive:HR14Rules@proximus.modulusmongo.net:27017/Y8jyguwu');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function callback () {
@@ -18,6 +18,11 @@ db.once('open', function callback () {
   console.log("Connected to remote MongoDB database");
 });
 
+function find (collec, query, callback, number) {
+    mongoose.connection.db.collection(collec, function (err, collection) {
+    collection.find(query).limit(number).toArray(callback);
+    });
+}
 //ROUTES FOR OUR API
 // -----------------------------------------------------
 
@@ -35,13 +40,16 @@ router.route('/events')
      })
      //get all events from DB limit to 20
      .get(function(req,res){
-
+        console.log("Fetching events");
+        find('events',{},function(err,events){
+          res.send(events);
+        },15);
      });
 
      //create a new event
 router.route('/events/:event_id')
     .get(function(req,res){
-
+      res.sendFile(__dirname + '/public/creator.html');
     });
 
 
@@ -52,6 +60,7 @@ app.use('/api',router);
 io.on('connection', function(socket){
   console.log('a user connected');
   io.emit('info', { msg: 'Enjoy the decline' });
+
   socket.on('disconnect', function(){
     console.log('user disconnected');
   });
@@ -59,8 +68,13 @@ io.on('connection', function(socket){
     //recieve client data
   socket.on('client_comment', function(data){
     console.log(data.comment);
-    socket.broadcast.emit('new_comment',{msg : data.comment});
+    // socket.broadcast.emit('new_comment',{msg : data.comment});
   });
+
+  socket.on('creator_stream', function(data){
+    console.log("The Creator sent something", data);
+    socket.broadcast.emit('new_comment',{msg: data.comment});
+  })
 });
 
 
